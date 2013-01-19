7f3746c06f77a25a26b874265c42bdbc551ab7fd
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2013-01-18 22:14:40 -0500

    Basic navigation bar.

--- web/app/controllers/welcome_controller.rb
+++ web/app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
 
   def index
+    @current_tab = :home
+
     respond_to do |format|
       format.html
     end
--- web/app/helpers/application_helper.rb
+++ web/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def navigation(name, address)
+    style = if name == @current_tab
+              'class="active"'
+            else
+              'class=""'
+            end
+
+    raw "<li #{style}><a href=\"#{address}\">#{name.to_s.capitalize}</a></li>"
+  end
+
 end
