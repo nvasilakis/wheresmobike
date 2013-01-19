6458095b89eed92f9f740d676308e3dd6d67c170
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2013-01-19 00:24:37 -0500

    Very simple AJAX loading screen.

--- web/app/assets/javascripts/search.js
+++ web/app/assets/javascripts/search.js
@@ -1,11 +1,21 @@
 $(document)
     .delegate('#search-form', 'ajax:before', function () {
-        console.log('before');
-        return true;
-    })
-    .delegate('#search-form', 'ajax:success', function () {
-        console.log('success');
+        var form = $(this),
+            loadingBox = $('#loading-search');
+        form.fadeOut(400, function () {
+            loadingBox.fadeIn(400, function () {
+                console.log('finished animation');
+            });
+        });
     })
     .delegate('#search-form', 'ajax:complete', function () {
+        var loadingBox = $('#loading-search');
         console.log('complete');
+        loadingBox.fadeOut();
+    })
+    .delegate('#search-form', 'ajax:success', function () {
+        var form = $(this),
+            loadingBox = $('#loading-search');
+        console.log('success');
+        loadingBox.fadeOut();
     });
