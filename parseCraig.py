0f7261009f5948f73cc73d6ee5c4cb24e73201e6
Leonidas Lampropoulos <leonidaslamp@hotmail.com>
2013-01-19 00:13:36 -0500

    Ground Data

--- parseCraig.py
+++ parseCraig.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 from lxml import etree, html
-from urllib import urlencode
+from urllib import urlencode,urlretrieve
 from urllib2 import Request, urlopen
+from os import chdir
+
+DOWNPATH="vision/data/"
+chdir(DOWNPATH)
 
 CRAIG_ADDR = "http://www.craigslist.org/about/sites"
 craig_response = urlopen(CRAIG_ADDR)
@@ -11,26 +15,31 @@ craig_root = html.parse(craig_response)
 us = craig_root.xpath('//div[@class="colmask" and h1/a[@name="US"]]')
 if us != []:
     cities = us[0].xpath('.//li/a/@href')
-    for city in cities:
+    for city in cities[:4]:
         addr = city + "/bia"
         city_response = urlopen(addr)
         city_root = html.parse(city_response)
         rows = city_root.xpath('//p[@class="row"]')
 
-        for row in rows[1:2]:
+        for row in rows[:10]:
 
             url = row.xpath('a/@href')[0]
             row_response = urlopen(url)
             row_root = html.parse(row_response)
             body = row_root.xpath('//section[@id="postingbody"]')
-            imgUrl = row_root.xpath('//img[@id="iwi"]/@src')
+#            imgUrl = row_root.xpath('//img[@id="iwi"]/@src') # Single Picture
+            imgUrl = row_root.xpath('//div[@id="thumbs"]/a/@href')
             date = row_root.xpath('//date')
 
-            # Do something with them
-            if body != []:
-                print body[0].text
+            print " -"
+            print '   url: "' + url + '"'
+            print '   color: '
+            print '   type: '
+            print '   make_or_model: '
             if imgUrl != []:
-                print imgUrl[0]
-            if date != []:
-                print date[0].text
+                print '   images: '
+                for img in imgUrl:
+                    name = img.split('/')[-1]
+                    print '     - "' + name + '"'
+                    urlretrieve(img, name)
 
