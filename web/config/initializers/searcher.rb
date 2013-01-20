5da368209d2505b5ec764a4b59cc2455d4142e54
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2013-01-20 01:18:58 -0500

    Move Searcher config.

--- web/config/initializers/searcher.rb
+++ web/config/initializers/searcher.rb
@@ -1,7 +1,7 @@
 require 'searcher'
 
 Searcher.solr_url AppConfig.solr_url
-Searcher.inpipe_name('/tmp/WheresMoBikeIn')
-Searcher.outpipe_name('/tmp/WheresMoBikeOut')
-Searcher.comparer_command('ls')
+Searcher.inpipe_name AppConfig.inpipe_name
+Searcher.outpipe_name AppConfig.outpipe_name
+Searcher.comparer_command AppConfig.comparer_command
 Searcher.start
