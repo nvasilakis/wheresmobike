d2211033ac21b25f4b536158002fb475c880b2d7
Arthur Azevedo de Amorim <arthur.aa@gmail.com>
2013-01-20 07:30:01 -0500

    Save uploaded picture.

--- web/app/controllers/search_controller.rb
+++ web/app/controllers/search_controller.rb
@@ -3,13 +3,25 @@ require 'searcher'
 class SearchController < ApplicationController
   def search
     @picture = params[:picture]
+    if @picture.present?
+      old_name = @picture.tempfile.path
+      new_dir = "#{Rails.root}/public/uploaded"
+      new_name = "#{SecureRandom.hex 16}#{@picture.original_filename}"
+      @picture = "#{new_dir}/#{new_name}"
+      FileUtils.cp(old_name, @picture)
+    end
+
     @description = params[:description]
     @date = Date.strptime(params[:date], '%m/%d/%Y').to_time
 
-    @results =
-      Searcher.search(:picture => @picture,
-                      :description => @description,
-                      :date => @date)
+    @response = {
+      :search => {
+        :picture => "/uploaded/#{File.basename @picture}"
+      },
+      :results => Searcher.search(:picture => @picture,
+                                  :description => @description,
+                                  :date => @date)
+    }
 
     # uncomment to test waiting screen
     sleep 5
